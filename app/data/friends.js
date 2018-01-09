2ca4490ff382290f797e103d03c6a4c42cc7bea0
patrzykont <aaronpatrzykont@Aarons-MacBook-Air.local>
2018-01-09 17:31:58 -0600

    new

---
+++ app/data/friends.js
@@ -0,0 +1,59 @@
+var friends = [ {
+    "name" : "Jerry",
+    "photo" : "http://static-cms.legacy.com/sites/default/files/styles/og_share_image/public/hero-jerry-garcia-getty.jpg?itok=-HduSQx1",
+    "scores" : [5,4,2,5,1,1,2,1,3,5]
+}, {
+    "name" : "Rodrigo",
+    "photo" : "https://metrouk2.files.wordpress.com/2017/08/rodrigo.jpg",
+    "scores" : [3,4,2,4,4,1,1,2,3,4]
+}, {
+    "name" : "Goku",
+    "photo" : "http://media.comicbook.com/2017/07/goku-dragon-ball-super-1006360-1280x0.png",
+    "scores" : [5,5,5,5,5,5,5,5,5,5]
+}, {
+    "name" : "Samson",
+    "photo" : "https://assetsnffrgf-a.akamaihd.net/assets/m/502012441/univ/art/502012441_univ_lsr_lg.jpg",
+    "scores" : [5,2,4,3,3,2,1,3,3,4]
+}, {
+    "name" : "Misty",
+    "photo" : "https://cdn.costumewall.com/wp-content/uploads/2015/10/misty-pokemon.jpg",
+    "scores" : [4,1,5,3,4,5,2,3,4,5]
+}, {
+    "name" : "Pepper",
+    "photo" : "https://i.pinimg.com/originals/2b/f6/ac/2bf6ace31bddb2ec75dae303154b3b94.jpg",
+    "scores" : [4,1,5,3,4,5,2,3,4,5]
+}, {
+    "name" : "Carmen",
+    "photo" : "https://www.theplace2.ru/archive/carmen_electra/img/carmen_electra_grand_opening_of_cherry_boom_boom_at_the_tropicana_las_vegas_september_2016_1.jpg",
+    "scores" : [3,4,2,4,4,1,1,2,3,4]
+}, {
+    "name" : "Susan",
+    "photo" : "https://static.independent.co.uk/s3fs-public/thumbnails/image/2016/01/18/10/Susan-Sarandon.jpg",
+    "scores" : [5,4,2,5,1,1,2,1,3,5]
+}, {
+    "name" : "Jim",
+    "photo" : "https://tribzap2it.files.wordpress.com/2016/10/the-office-jim-halpert-john-krasinski-1.jpg?w=1200",
+    "scores" : [1,5,1,3,5,1,4,3,2,1]
+}, {
+    "name" : "Kobe",
+    "photo" : "http://www.trbimg.com/img-565b989f/turbine/la-sp-ln-kobe-bryant-announces-retirement-after-season-20151129",
+    "scores" : [1,3,3,1,2,4,4,1,3,1]
+}, {
+    "name" : "Tom",
+    "photo" : "https://vignette.wikia.nocookie.net/stephenking/images/0/0b/Tom-hanks.jpg/revision/latest?cb=20170504034642",
+    "scores" : [5,2,4,3,3,2,1,3,3,4]
+}, {
+    "name" : "Jessica",
+    "photo" : "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c2/Jessica_Alba_SDCC_2014.jpg/1200px-Jessica_Alba_SDCC_2014.jpg",
+    "scores" : [1,1,1,1,1,1,1,1,1,1]
+}, {
+    "name" : "Bernie",
+    "photo" : "https://thelibertarianrepublic.com/wp-content/uploads/2016/02/Bernie-Sanders-AP77174442780.jpg",
+    "scores" : [2,3,5,5,3,5,2,1,3,2]
+}, {
+    "name" : "Babs",
+    "photo" : "https://pre00.deviantart.net/4b57/th/pre/f/2014/106/7/a/babs_bunny_by_tetsuwanatom-d7ep4v9.png",
+    "scores" : [2,3,5,5,3,5,2,1,3,2]
+}];
+
+module.exports = friends;
